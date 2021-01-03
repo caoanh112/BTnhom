26e4890c3b7fa887a875ae52f5216253701052ae
Phuong <Phuong20002002@gmail.com>
2021-01-03 21:08:20 +0700

    buttton

--- BTnhom/frm_login.Designer.cs
+++ BTnhom/frm_login.Designer.cs
@@ -29,12 +29,32 @@ namespace BTnhom
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "frm_login";
             this.Text = "frm_login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
