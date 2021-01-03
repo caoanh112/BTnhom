6ab213c75d87014bc4e09050307cafe487c25e75
Phuong <Phuong20002002@gmail.com>
2021-01-03 22:41:50 +0700

    update add test

--- BTnhom/Program.cs
+++ BTnhom/Program.cs
@@ -14,6 +14,7 @@ namespace BTnhom
         [STAThread]
         static void Main()
         {
+            // Phương
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
